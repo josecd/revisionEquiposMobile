ee34ff59fe91c6e05167b415bd8d3d88fcfa76c4
josecd <44279311+josecd@users.noreply.github.com>
2023-06-06 16:25:25 -0500

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'revisionEquiposMobile',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
